989cc28dffdffc38567189e2bebe99e31973e7a9
Oli <85398482+Redflue@users.noreply.github.com>
2024-06-02 16:09:28 -0400

    fixe null position

--- PlaneTP/Simulator/Model/ClientSupport.cs
+++ PlaneTP/Simulator/Model/ClientSupport.cs
@@ -8,10 +8,8 @@ namespace Simulator.Model;
 
 public abstract class ClientSupport : Client
 {
-    private Position _position;
-    public Position Position { get; set; }
     protected ClientSupport(Position position)
     {
-        _position = position;
+        Position = position;
     }
 }
