bc3cda2161698a92438347481ef2348d0e2497e4
Jacob Lévesque <157502870+feywilddd@users.noreply.github.com>
2024-06-01 20:04:57 -0400

    fixed weird merge

--- PlaneTP/Simulator/Model/FlyingTransport.cs
+++ PlaneTP/Simulator/Model/FlyingTransport.cs
@@ -3,9 +3,26 @@
 public class FlyingTransport : Flying
 {
     protected ClientTransport _client;
+    
 
     public FlyingTransport(Plane plane, ClientTransport client) : base(plane)
     {
         _client = client;
+    }
+    public void newPosition()
+    {
+        float t = 0.0001f * _plane.Speed % 1f;
+        int x = (int)((1 - t) * _plane.Airport.Position.X + t * _client.Destination.Position.X);
+        int y = (int)((1 - t) * _plane.Airport.Position.Y + t * _client.Destination.Position.Y);
+        _plane.Position = new Position(x, y);
+    }
+    
+    public override void TimeStep()
+    {
+        newPosition();
+        if (_plane.Airport.Position.X == _client.Destination.Position.X && _plane.Airport.Position.Y == _client.Destination.Position.Y)
+        {
+            _plane.State = new Deboarding((PlaneTransport)_plane,  _client);
+        }
     }
 }
